4807436b35888c2ecba4352af81a2ea425f69cca
Jason Matthew <jasonm4130@gmail.com>
2021-11-01 14:14:12 +1000

    initial commit

---
+++ src/pages/404.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { Link } from 'gatsby';
+
+// markup
+const NotFoundPage = () => <main />;
+
+export default NotFoundPage;
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+// This should be moved into it's own template once created
+import 'normalize.css';
+
+import * as styles from '../styles.module.scss';
+
+// markup
+const IndexPage = () => (
+  <main>
+    <h1 className={styles.title}>This is a test</h1>
+  </main>
+);
+
+export default IndexPage;
